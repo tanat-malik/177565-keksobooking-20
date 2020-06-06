06f29562ceb1adfec7664de1c47703b7d8f342d9
Tanat <tanat.malik.93@mail.ru>
2020-06-06 12:13:11 +0600

    Правка кода по замечаниям ДЗ больше деталей (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
 'use strict';
 
 // Переменные для генерирования данных
-var pinAmount = 8;
-var apartmentType = [
+var PIN_AMOUNT = 8;
+var TYPES = [
   'palace',
   'flat',
   'house',
   'bungalo'
 ];
-var apartmentPrice = [
+var PRICES = [
   10000,
   20000,
   30000,
   40000,
   50000
 ];
-var apartmentRooms = [
+var ROOMS = [
   1,
   2,
   3,
   100
 ];
-var apartmentGuests = [
+var GUESTS = [
   1,
   2,
   3
 ];
-var apartmentCheckin = [
+var CHECKIN = [
   '12:00',
   '13:00',
   '14:00'
 ];
-var apartmentCheckout = [
+var CHECKOUT = [
   '12:00',
   '13:00',
   '14:00'
 ];
-var apartmentFeatures = [
+var FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -44,7 +44,7 @@ var apartmentFeatures = [
   'elevator',
   'conditioner'
 ];
-var apartmentPhotos = [
+var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
@@ -69,10 +69,10 @@ function getRandomArrElem(arr) {
   return arr[getRandomValue(0, arr.length - 1)];
 }
 
-// Функция для выбора случайного количество элементов из массива
+// Функция для выбора случайного количество элементов из массив
 function getRandomNumOfElemFromArr(arr) {
   var elements = [];
-  for (var i = 0; i < Math.round(Math.random() * arr.length + 1); i++) {
+  for (var i = 0; i < getRandomValue(0, arr.length - 1); i++) {
     var option = getRandomArrElem(arr);
     if (elements.indexOf(option) === -1) {
       elements.push(option);
@@ -92,17 +92,17 @@ function generateMocks(counter) {
         'avatar': 'img/avatars/user0' + (i + 1) + '.png',
       },
       'offer': {
-        'title': getRandomArrElem(apartmentType),
+        'title': getRandomArrElem(TYPES),
         'address': locationX + ' ' + locationY,
-        'price': getRandomArrElem(apartmentPrice),
-        'type': getRandomArrElem(apartmentType),
-        'rooms': getRandomValue(1, apartmentRooms.length - 1),
-        'guests': getRandomValue(1, apartmentGuests.length - 1),
-        'checkin': getRandomArrElem(apartmentCheckin),
-        'checkout': getRandomArrElem(apartmentCheckout),
-        'features': getRandomNumOfElemFromArr(apartmentFeatures),
-        'description': getRandomArrElem(apartmentType),
-        'photos': getRandomArrElem(apartmentPhotos),
+        'price': getRandomArrElem(PRICES),
+        'type': getRandomArrElem(TYPES),
+        'rooms': getRandomValue(1, ROOMS.length - 1),
+        'guests': getRandomValue(1, GUESTS.length - 1),
+        'checkin': getRandomArrElem(CHECKIN),
+        'checkout': getRandomArrElem(CHECKOUT),
+        'features': getRandomNumOfElemFromArr(FEATURES),
+        'description': getRandomArrElem(TYPES),
+        'photos': getRandomArrElem(PHOTOS),
       },
       'location': {
         'x': locationX,
@@ -115,7 +115,7 @@ function generateMocks(counter) {
 }
 
 // На основе данных, полученных из функции generateMocks, клону шаблона pinElement задаем метки координат и изображений
-function setPinValue(data) {
+function getPinTemplate(data) {
   var pinElement = templatePin.cloneNode(true);
   var pinIcon = pinElement.querySelector('img');
   pinElement.style = 'left: ' + data.location.x + 'px; top: ' + data.location.y + 'px;';
@@ -128,9 +128,9 @@ function setPinValue(data) {
 function renderPins(pinsData) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < pinsData.length; i++) {
-    fragment.appendChild(setPinValue(pinsData[i]));
+    fragment.appendChild(getPinTemplate(pinsData[i]));
   }
   mapPins.appendChild(fragment);
 }
 
-renderPins(generateMocks(pinAmount));
+renderPins(generateMocks(PIN_AMOUNT));
