195ae4004fd92d874d9c7321be6de42269a80e7a
Tanat <tanat.malik.93@mail.ru>
2020-06-09 13:50:12 +0600

    ДЗ - Больше деталей(часть 2)

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ function getRandomArrElem(arr) {
 // Функция для выбора случайного количество элементов из массив
 function getRandomNumOfElemFromArr(arr) {
   var elements = [];
-  for (var i = 0; i < getRandomValue(0, arr.length - 1); i++) {
+  for (var i = 0; i < getRandomValue(1, arr.length - 1); i++) {
     var option = getRandomArrElem(arr);
     if (elements.indexOf(option) === -1) {
       elements.push(option);
@@ -102,7 +102,7 @@ function generateMocks(counter) {
         'checkout': getRandomArrElem(CHECKOUT),
         'features': getRandomNumOfElemFromArr(FEATURES),
         'description': getRandomArrElem(TYPES),
-        'photos': getRandomArrElem(PHOTOS),
+        'photos': getRandomNumOfElemFromArr(PHOTOS),
       },
       'location': {
         'x': locationX,
@@ -134,3 +134,75 @@ function renderPins(pinsData) {
 }
 
 renderPins(generateMocks(PIN_AMOUNT));
+
+/*
+  ДЗ - Больше деталей (часть 2)
+*/
+
+// Находим шаблон #card
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+
+// Функция для склонения по падежам слова комната
+function getRoomsСase(value) {
+  if (value === 1) {
+    return value + ' комната';
+  } else if (value === 5) {
+    return value + ' комнат';
+  } else {
+    return value + ' комнаты';
+  }
+}
+
+// Функция для склонения по падежам слова гость
+function getGuestСase(value) {
+  if (value === 1) {
+    return value + ' гостя';
+  } else {
+    return value + ' гостей';
+  }
+}
+
+// Функция для создания попапа
+function createCards(card) {
+  var cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.popup__title').textContent = card.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
+  if (card.offer.type === 'palace') {
+    cardElement.querySelector('.popup__type ').textContent = 'Дворец';
+  } else if (card.offer.type === 'flat') {
+    cardElement.querySelector('.popup__type ').textContent = 'Квартира';
+  } else if (card.offer.type === 'house') {
+    cardElement.querySelector('.popup__type ').textContent = 'Дом';
+  } else {
+    cardElement.querySelector('.popup__type ').textContent = 'Бунгало';
+  }
+  cardElement.querySelector('.popup__text--capacity').textContent = getRoomsСase(card.offer.rooms) + ' для ' + getGuestСase(card.offer.guests);
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+  cardElement.querySelector('.popup__features').textContent = card.offer.features;
+
+  // Так как данные по offer.description отсутствуют, воспользуемся данными из шаблона #card
+  // cardElement.querySelector('.popup__description').textContent = card.offer.description;
+
+  cardElement.querySelector('.popup__avatar').src = card.author.avatar;
+  var popupPhotos = cardElement.querySelector('.popup__photos');
+  var photo = popupPhotos.querySelector('img');
+  for (var i = 0; i < card.offer.photos.length; i++) {
+    var photoElem = photo.cloneNode(true);
+    photoElem.src = card.offer.photos[i];
+    popupPhotos.appendChild(photoElem);
+  }
+  photo.remove();
+
+  return cardElement;
+}
+
+// Функция для рендеринга попапа на карте
+function renderCards(cardData) {
+  var fragment = document.createDocumentFragment();
+  fragment.appendChild(createCards(cardData[0]));
+  map.appendChild(fragment);
+}
+
+renderCards(generateMocks(PIN_AMOUNT));
