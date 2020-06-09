a6d319320d1a04f5b619cfaa64511a498b81f49f
Tanat <tanat.malik.93@mail.ru>
2020-06-09 17:31:21 +0600

    Правка замечаний по ДЗ - больше деталей(часть 2)

--- js/main.js
+++ js/main.js
@@ -170,17 +170,17 @@ function createCards(card) {
   cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
   cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
   if (card.offer.type === 'palace') {
-    cardElement.querySelector('.popup__type ').textContent = 'Дворец';
+    cardElement.querySelector('.popup__type').textContent = 'Дворец';
   } else if (card.offer.type === 'flat') {
-    cardElement.querySelector('.popup__type ').textContent = 'Квартира';
+    cardElement.querySelector('.popup__type').textContent = 'Квартира';
   } else if (card.offer.type === 'house') {
-    cardElement.querySelector('.popup__type ').textContent = 'Дом';
+    cardElement.querySelector('.popup__type').textContent = 'Дом';
   } else {
-    cardElement.querySelector('.popup__type ').textContent = 'Бунгало';
+    cardElement.querySelector('.popup__type').textContent = 'Бунгало';
   }
   cardElement.querySelector('.popup__text--capacity').textContent = getRoomsСase(card.offer.rooms) + ' для ' + getGuestСase(card.offer.guests);
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
-  cardElement.querySelector('.popup__features').textContent = card.offer.features;
+  cardElement.querySelector('.popup__features').textContent = card.offer.features.join(', ');
 
   // Так как данные по offer.description отсутствуют, воспользуемся данными из шаблона #card
   // cardElement.querySelector('.popup__description').textContent = card.offer.description;
